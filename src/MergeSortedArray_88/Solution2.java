c2fd04e098290c3d319e7ba54812dbe43d736815
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-07-13 17:37:27 +0700

    Finish problem 88. merge 2 sorted arrays

--- src/MergeSortedArray_88/Solution2.java
+++ src/MergeSortedArray_88/Solution2.java
@@ -3,40 +3,40 @@ package MergeSortedArray_88;
 public class Solution2 {
 
     public static void main(String[] args) {
-        /*int m = 3;
+        int m = 3;
         int n = 3;
         int nums1[] = {1, 2, 3, 0, 0, 0};
-        int nums2[] = {2, 5, 6};*/
-
-        int m = 0;
-        int n = 1;
-        int nums1[] = {0};
-        int nums2[] = {1};
+        int nums2[] = {2, 5, 6};
         merge(nums1, m, nums2, n);
     }
 
     public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        // Start from the end of both arrays
+        int p1 = m - 1; // Pointer for nums1
+        int p2 = n - 1; // Pointer for nums2
+        int p = m + n - 1; // Pointer for the last position in nums1
 
-        //The number of elements in nums1, nums2 and the actually in nums1 (n+m)
-        int e1 = m - 1;
-        int e2 = n - 1;
-        int e3 = m + n - 1;
-
-        while (e1 >= 0 && e2 >= 0 && e3 >= 0) {
-            if (nums2[e2] > nums1[e1]) {
-                nums1[e3] = nums2[e2];
-                e2--;
+        // While there are still elements to compare
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) {
+                nums1[p] = nums1[p1];
+                p1--;
             } else {
-                nums1[e3] = nums1[e1];
-                e1--;
+                nums1[p] = nums2[p2];
+                p2--;
             }
-            e3--;
+            p--;
+        }
+
+        // If there are still elements in nums2, copy them over
+        while (p2 >= 0) {
+            nums1[p] = nums2[p2];
+            p2--;
+            p--;
         }
 
         for (int i = 0; i < n+m; i++) {
             System.out.println(nums1[i]);
         }
-
-
     }
 }
