9bcaae3081b4762417eb60db5aa6082c4a17e8b2
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-01 15:58:48 +0700

    setup

---
+++ src/SearchInsertPosition_35/Solution.java
@@ -0,0 +1,16 @@
+package SearchInsertPosition_35;
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[] {1, 3, 5, 6};
+        int target = 5;
+        System.out.println(nums.length);
+        System.out.println(searchInsert(nums, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        return 1;
+    }
+}
