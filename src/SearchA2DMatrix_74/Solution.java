243c8cb009955b17049cb6be57702db0b5f4529d
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-01 17:13:52 +0700

    Finish

--- src/SearchA2DMatrix_74/Solution.java
+++ src/SearchA2DMatrix_74/Solution.java
@@ -3,24 +3,44 @@ package SearchA2DMatrix_74;
 public class Solution {
     public static void main(String[] args) {
         //Declare 2D array
-        int[][] nums = new int[][]{
+        int[][] matrix = new int[][]{
                 {1,3,5,7},
                 {10,11,16,20},
                 {23,30,34,60}
         };
 
-        int target = 3;
+        int target = 204;
+        System.out.println(searchMatrix(matrix, target));
 
+        /*
         //Print 2D array
         for (int[] i : nums) {
             for (int j : i)
                 System.out.print(j + " ");
             System.out.println();
-        }
+        }*/
     }
 
     public static boolean searchMatrix(int[][] matrix, int target) {
+        for (int[] i : matrix) {
+            if (binarySearch(i, target))
+                return true;
+        }
+        return false;
+    }
 
+    public static boolean binarySearch(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = (left + right) / 2;
+        while (left <= right) {
+            if (nums[mid] == target)
+                return true;
+            else if (nums[mid] > target)
+                right = mid - 1;
+            else left = mid + 1;
+            mid = (left + right) / 2;
+        }
         return false;
     }
 }
