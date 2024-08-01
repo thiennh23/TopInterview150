1ba33e52eb5a66037d2b87313c28f19c0ff3fde8
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-01 16:14:06 +0700

    reach the index ok

--- src/SearchInsertPosition_35/Solution.java
+++ src/SearchInsertPosition_35/Solution.java
@@ -4,13 +4,22 @@ public class Solution {
     public static void main(String[] args) {
         int[] nums = new int[] {1, 3, 5, 6};
         int target = 5;
-        System.out.println(nums.length);
         System.out.println(searchInsert(nums, target));
     }
 
     public static int searchInsert(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        return 1;
+        int mid = (start + end) / 2;
+        while (start < end) {
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] > target)
+                end = mid - 1;
+            else if (nums[mid] < target)
+                start = mid + 1;
+            mid = (start + end) / 2;
+        }
+        return -1;
     }
 }
