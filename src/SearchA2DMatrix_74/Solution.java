501ad4c6c2e29b5a184d6277da9e1e70ff7998eb
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2024-08-01 17:51:40 +0700

    add null safety for column

--- src/SearchA2DMatrix_74/Solution.java
+++ src/SearchA2DMatrix_74/Solution.java
@@ -3,13 +3,16 @@ package SearchA2DMatrix_74;
 public class Solution {
     public static void main(String[] args) {
         //Declare 2D array
-        int[][] matrix = new int[][]{
+        /*int[][] matrix = new int[][]{
                 {1,3,5,7},
                 {10,11,16,20},
                 {23,30,34,60}
+        };*/
+        int[][] matrix = new int[][]{
+                {1}
         };
 
-        int target = 3;
+        int target = 04;
         System.out.println(searchMatrix(matrix, target));
 //        System.out.println(suitableRow(test, target));
         /*
@@ -40,6 +43,10 @@ public class Solution {
     //So, we will have the complexity: Log(M)xLog(N) which is very great
     public static boolean searchMatrix(int[][] matrix, int target) {
         int row = suitableRow(matrix, target);
+        //Means that the column is null, just have 1 dimension
+        if (row == -1) {
+            return false;
+        }
         return binarySearch(matrix[row], target);
     }
 
